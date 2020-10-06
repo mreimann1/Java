8cdfe6b14c6aade0af491d19ecc763c7ee33a86c
Marques Reimann <marques_reimann@yahoo.com>
2020-10-06 10:13:38 -0700

    countwords.cpp updated

--- CountWords/countwords.cpp
+++ CountWords/countwords.cpp
@@ -1,10 +1,11 @@
 /**
- * countwords.cpp
+ * @file: countwords.cpp
  *
- * @description: This program reads in all words from specified files and
+ * @brief: This program reads in all words from specified files and
  *      prints each word and the number of times that words was used.
  * @arguments: A list of filenames. If this is not passed from the command line
  *      then it takes these arguments from standard input
+ * @author: Marques Reimann
  */
 
 #include <cctype>
@@ -15,41 +16,39 @@
 using namespace std;
 
 /**
- * Function to count the number of words in a string
+ * @brief: Count the number of words in a string
+ * @param: string in_str - string to be parsed
+ *         map<string,int>& words - map of words to their counts
+ * @return: Void
  * Some code integrated from https://stackoverflow.com/questions/3672234/c-function-to-count-all-the-words-in-a-string
  *  and from https://stackoverflow.com/questions/347949/how-to-convert-a-stdstring-to-const-char-or-char
  */
-
-void num_words(string in_str, map<string,int>& words)
-{
-
+void num_words(string in_str, map<string,int>& words) {
+  // Copy the input string to a char*
   char * str = new char[in_str.size() +1];
   copy(in_str.begin(), in_str.end(), str);
   str[in_str.size()] = '\0';
 
+  // Check for a null string
   if (str == NULL)
     throw "String is null.";
 
+  // Initialize variables
   bool inSpaces = true;
   int numWords = 0;
-
-  // Use int to keep track of start and end of string
-  int start = 0; // start at the beginning
-  int end = 0;
+  int start = 0; // Track the start of a word
+  int end = 0;  // Track the end of a word
   string word;
 
-  while (*str != '\0')
-  {
-    if (std::isspace(*str))
-    {
+  // Iterate through input string and find each word
+  while (*str != '\0') {
+    if (std::isspace(*str)) {
       word = in_str.substr(start, end-start);
-      cout << word << " found in text\n";
       words[word]++;
       inSpaces = true;
       start = end+1;
     }
-    else if (inSpaces)
-    {
+    else if (inSpaces) {
       numWords++;
       inSpaces = false;
     }
@@ -60,46 +59,47 @@ void num_words(string in_str, map<string,int>& words)
 }
 
 /**
- * Functio to open a file, then call num_words to find the number of words in
- * that file.
+ * @brief: Open a file, then call num_words to find the number of words in
+ *         that file.
+ * @param: string filename - name of file to be opened
+ *         map<string,int>& words - map of words to their counts
+ * @return: Void
  */
-void open_and_count(string arg, map<string, int>& words) {
-  cout << arg << " is being opened...\n";
+void open_and_count(string filename, map<string, int>& words) {
   ifstream f_in;
-  f_in.open(arg);
-
+  f_in.open(filename);
   string line;
   if (f_in.is_open()) {
     while ( getline(f_in, line)) {
-      cout << line << endl;
       num_words(line, words); // count each word and add to total
     }
     f_in.close();
   } else {
-    cout << "Unable to open file\n";
+    cout << "Unable to open file \"" << filename << "\".\n";
   }
 
   return;
 }
 
-// Type your code here, or load an example.
-int main(int args, char **argv)
-{
+/**
+ * @brief: Program entrypoint
+ * @param: Command line arguments. Expects texts files.
+ * @return Should not return
+ */
+int main(int args, char **argv) {
     // Add a map which will be used by the whole program
     map<string, int> words;
 
     // Iterate through arguments
     for (int i = 1; i < args; i++) {
-      cout << argv[i] << " ";
       string filename = argv[i];
-
       cout << "filename: " << filename << endl;
       open_and_count(filename, words);
     }
 
     // Print the wrods and their counts
     for(auto elem : words) {
-      cout << elem.first << " " << elem.second << "\n";
+      cout << "\"" << elem.first << "\" has a count of " << elem.second << ".\n";
     }
     return 0;
 }
