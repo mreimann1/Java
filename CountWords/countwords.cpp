4c22b4e79e881713488cbe22712c7525cab11a08
Marques Reimann <marques_reimann@yahoo.com>
2020-10-05 22:06:40 -0700

    countwords.cpp updated

--- CountWords/countwords.cpp
+++ CountWords/countwords.cpp
@@ -6,72 +6,93 @@
  * @arguments: A list of filenames. If this is not passed from the command line
  *      then it takes these arguments from standard input
  */
-#include <bits/stdc++.h>
-##include <cctype>
+
+#include <cctype>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <map>
 using namespace std;
 
-// Function to count words
-// Counts the number of spaces or newlines and then adds 1
-void num_words(string str, int *result)
+/**
+ * Function to count the number of words in a string
+ * Some code integrated from https://stackoverflow.com/questions/3672234/c-function-to-count-all-the-words-in-a-string
+ *  and from https://stackoverflow.com/questions/347949/how-to-convert-a-stdstring-to-const-char-or-char
+ */
+
+void num_words(string in_str, int* result)
 {
-    //Base case: String is empty;
-    if (str.size() == 0)
-    {
-        *result = 0;
-        return;
-    }
 
-    for (auto x : str)
-    {
-        //if there's a space or newline, then there's a string
-        if (x == ' ' || x == '\n')
-        {
-            *result = *result + 1;
-        }
-    }
+  char * str = new char[in_str.size() +1];
+  copy(in_str.begin(), in_str.end(), str);
+  str[in_str.size()] = '\0';
 
-    *result = *result + 1;
-    return;
+  if (str == NULL)
+    throw "String is null.";
+
+  bool inSpaces = true;
+  int numWords = 0;
+
+  while (*str != '\0')
+  {
+    if (std::isspace(*str))
+    {
+      inSpaces = true;
+    }
+    else if (inSpaces)
+    {
+      numWords++;
+      inSpaces = false;
+    }
+    ++str;
+  }
+
+  *result = numWords;
+  return;
 }
 
-int CountWords(const char* str)
-{
-   if (str == NULL)
-      return error_condition;  // let the requirements define this...
+/**
+ * Functio to open a file, then call num_words to find the number of words in
+ * that file.
+ */
+void open_and_count(string arg, int* result, map<string, int>* words) {
+  cout << arg << " is being opened...\n";
+  ifstream f_in;
+  f_in.open(arg);
 
-   bool inSpaces = true;
-   int numWords = 0;
+  string line;
+  if (f_in.is_open()) {
+    while ( getline(f_in, line)) {
+      cout << line << endl;
+      int line_count;
+      num_words(line, &line_count); // count each word and add to total
+      *result += line_count;
+    }
+    f_in.close();
+  } else {
+    cout << "Unable to open file\n";
+  }
 
-   while (*str != '\0')
-   {
-      if (std::isspace(*str))
-      {
-         inSpaces = true;
-      }
-      else if (inSpaces)
-      {
-         numWords++;
-         inSpaces = false;
-      }
-
-      ++str;
-   }
-
-   return numWords;
+  return;
 }
 
 // Type your code here, or load an example.
 int main(int args, char **argv)
 {
-    cout << "args: " << args << endl;
+    // Add a map which will be used by the whole program
+    map<string, int> words;
 
+    //TODO: add code to iterate through list of arguments
     for (int i = 0; i < args; i++)
     { //loop through arguments
         cout << argv[i] << " ";
     }
-
     int result = 0;
-    num_words("ab abc abcd", &result);
-    cout << "The count of words: " << result;
+    //num_words("ab abc abcd", &result);
+    string filename = argv[1];
+    //strncpy(filename, argv[1]);
+    cout << "filename: " << filename << endl;
+    open_and_count(filename, &result, &words);
+    cout << "The count of words: " << result << endl;
     return 0;
 }
