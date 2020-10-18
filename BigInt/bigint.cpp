7ae4fbbea2a1b6a40f9f4a216e9e711e34ad2e1a
Marques Reimann <marques_reimann@yahoo.com>
2020-10-18 11:37:18 -0700

    bigint +operator implemented

--- BigInt/bigint.cpp
+++ BigInt/bigint.cpp
@@ -17,52 +17,93 @@ bigint::bigint (const ubigint& uvalue_, bool is_negative_):
 }
 
 bigint::bigint (const string& that) {
-   is_negative = that.size() > 0 and that[0] == '_';
-   uvalue = ubigint (that.substr (is_negative ? 1 : 0));
+  is_negative = that.size() > 0 and that[0] == '_';
+  uvalue = ubigint (that.substr (is_negative ? 1 : 0));
 }
 
 bigint bigint::operator+ () const {
-   return *this;
+  return *this;
 }
 
 bigint bigint::operator- () const {
-   return {uvalue, not is_negative};
+  return {uvalue, not is_negative};
 }
 
 bigint bigint::operator+ (const bigint& that) const {
-   ubigint result = uvalue + that.uvalue;
-   return result;
+
+  // if their signs match :
+  //  add their uvalues
+  //  copy the sign
+  // else (their signs are different):
+  //  subtract the smaller from the larger
+  //  set the result sign to the sign of the bigger
+  // make sure zeros sign is positive
+
+  bigint result;
+
+  if (is_negative == that.is_negative) {
+    result.uvalue = uvalue + that.uvalue;
+    result.is_negative = is_negative;
+    return result;
+  }
+
+  // else (their signs are different)
+
+  bool this_less_than_that = uvalue < that.uvalue;
+  bool that_less_than_this = that.uvalue < uvalue;
+
+  if (this_less_than_that ) {
+    DEBUGF('+', uvalue << " < " << that.uvalue);
+    result.uvalue = that.uvalue - uvalue;
+    result.is_negative = that.is_negative;
+  }
+
+  else if (that_less_than_this) {
+    DEBUGF('+', uvalue << " > " << that.uvalue);
+    result.uvalue = uvalue - that.uvalue;
+    result.is_negative = is_negative;
+  }
+  else /* they have equal uvalues */ {
+    DEBUGF('+', uvalue << " == " << that.uvalue);
+    ubigint zero {0};
+    result.uvalue = zero;
+    result.is_negative = false;
+  }
+  //TESTING THE RESULT OF PREVIOIUS LOGIC
+  cout << "TESTING 'operator+': result: " << result << endl;  
+
+  return result;
 }
 
 bigint bigint::operator- (const bigint& that) const {
-   ubigint result = uvalue - that.uvalue;
-   return result;
+  ubigint result = uvalue - that.uvalue;
+  return result;
 }
 
 
 bigint bigint::operator* (const bigint& that) const {
-   bigint result = uvalue * that.uvalue;
-   return result;
+  bigint result = uvalue * that.uvalue;
+  return result;
 }
 
 bigint bigint::operator/ (const bigint& that) const {
-   bigint result = uvalue / that.uvalue;
-   return result;
+  bigint result = uvalue / that.uvalue;
+  return result;
 }
 
 bigint bigint::operator% (const bigint& that) const {
-   bigint result = uvalue % that.uvalue;
-   return result;
+  bigint result = uvalue % that.uvalue;
+  return result;
 }
 
 bool bigint::operator== (const bigint& that) const {
-   return is_negative == that.is_negative and uvalue == that.uvalue;
+  return is_negative == that.is_negative and uvalue == that.uvalue;
 }
 
 bool bigint::operator< (const bigint& that) const {
-   if (is_negative != that.is_negative) return is_negative;
-   return is_negative ? uvalue > that.uvalue
-                      : uvalue < that.uvalue;
+  if (is_negative != that.is_negative) return is_negative;
+  return is_negative ? uvalue > that.uvalue
+                     : uvalue < that.uvalue;
 }
 
 ostream& operator<< (ostream& out, const bigint& that) {
